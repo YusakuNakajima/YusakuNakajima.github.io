d17b1a43b47035c528129fdc08263c15c49edd72
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-12-31 18:58:48 -0500

    Update

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,8 @@
 
 export const SITE_TITLE = 'Yusaku Nakajima';
 export const SITE_DESCRIPTION = 'Research Portfolio';
+
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/yusaku-nakajima-b5898930b/'; // Placeholder
+export const FACEBOOK_URL = 'https://www.facebook.com/profile.php?id=100044302041042/'; // Placeholder
+export const X_URL = 'https://x.com/nakaji_001/'; // Placeholder, previously hardcoded to astro's
+export const GITHUB_URL = 'https://github.com/YusakuNakajima/'; // Placeholder, previously hardcoded to astro's
