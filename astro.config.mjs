5a64ad6242f7edb2d1b26609b29179f79afbd6c7
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-12-31 18:47:58 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://YusakuNakajima.github.io',
+  base: '/',
+  integrations: [mdx()],
 });
