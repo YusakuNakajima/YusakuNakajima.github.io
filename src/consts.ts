e05d5fa90e6a48ce5ae3da1f2a6d17b45a02e63e
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2026-04-26 16:29:47 -0400

    uodate

--- src/consts.ts
+++ src/consts.ts
@@ -8,3 +8,4 @@ export const LINKEDIN_URL = 'https://www.linkedin.com/in/yusaku-nakajima-b589893
 export const FACEBOOK_URL = 'https://www.facebook.com/profile.php?id=100044302041042/'; // Placeholder
 export const X_URL = 'https://x.com/nakaji_001/'; // Placeholder, previously hardcoded to astro's
 export const GITHUB_URL = 'https://github.com/YusakuNakajima/'; // Placeholder, previously hardcoded to astro's
+export const SCHOLAR_URL = 'https://scholar.google.co.jp/citations?user=k-UzcUgAAAAJ&hl=en';
---
+++ src/data/news.ts
@@ -0,0 +1,74 @@
+export type NewsItem = {
+	date: string;
+	dateJa: string;
+	html: string;
+	htmlJa: string;
+};
+
+export const newsItems: NewsItem[] = [
+	{
+		date: 'Apr. 20, 2026',
+		dateJa: '2026年4月20日',
+		html: 'Yusaku Nakajima (D3) contributed an invited commentary article on the quantification and automation of powder processes in materials science to the May 2026 issue of <em>Jikken Igaku</em>. <a href="https://www.yodosha.co.jp/yodobook/book/9784758126038/">Link</a>',
+		htmlJa:
+			'中島優作（D3）が材料科学における粉体プロセスの定量化と自律化に関する解説記事を、生命科学・医学系の専門誌「実験医学」2026年5月号に寄稿しました。 <a href="https://www.yodosha.co.jp/yodobook/book/9784758126038/">Link</a>',
+	},
+	{
+		date: 'Apr. 3, 2026',
+		dateJa: '2026年4月3日',
+		html: 'Yusaku Nakajima (D3) and his career path were featured in the Kosen Career special issue of <em>Nikkan Kogyo Shimbun</em>. <a href="https://www.nikkan.co.jp/">Link</a>',
+		htmlJa:
+			'中島優作（D3）の研究と経歴が日刊工業新聞の高専キャリア特集に掲載されました。 <a href="https://www.nikkan.co.jp/">Link</a>',
+	},
+	{
+		date: 'Jan. 8, 2026',
+		dateJa: '2026年1月8日',
+		html: 'An interview with Yusaku Nakajima (D3) was published on the University of Osaka Faculty of Engineering admissions site, <em>Meccha! Handai Kogakubu</em>. <a href="https://www.eng.osaka-u.ac.jp/prospective/family/3045/">Link</a>',
+		htmlJa:
+			'大阪大学工学部の受験生向けサイト「めっちゃ！阪大工学部」に、中島優作（D3）のインタビュー記事が掲載されました。 <a href="https://www.eng.osaka-u.ac.jp/prospective/family/3045/">Link</a>',
+	},
+	{
+		date: 'Dec. 20, 2025',
+		dateJa: '2025年12月20日',
+		html: 'Yusaku Nakajima (D3) gave a presentation at the Boston Japanese Researchers Networking Meeting. <a href="https://www.boston-researchers.jp/">Link</a>',
+		htmlJa:
+			'中島優作（D3）がボストン日本人研究者交流会で発表を行いました。 <a href="https://www.boston-researchers.jp/">Link</a>',
+	},
+	{
+		date: 'Oct. 2025',
+		dateJa: '2025年10月',
+		html: 'Started as a Research Trainee at <a href="https://snr.spl.harvard.edu/">Brigham and Women\'s Hospital, Harvard Medical School</a>.',
+		htmlJa:
+			'<a href="https://snr.spl.harvard.edu/">ハーバード大学医学大学院 ブリガム・アンド・ウィメンズ病院</a>で学生研究員を開始。',
+	},
+	{
+		date: 'Sep. 2025',
+		dateJa: '2025年9月',
+		html: 'Delivered an invited talk on powder handling technologies at the Ceramic Society of Japan Autumn Symposium.',
+		htmlJa: 'セラミックス協会秋季シンポジウムで粉体ハンドリング技術に関する招待講演を実施。',
+	},
+	{
+		date: 'Sep. 2025',
+		dateJa: '2025年9月',
+		html: 'Delivered an invited talk at IIBMP 2025 on experimental automation and powder process informatics.',
+		htmlJa: 'IIBMP 2025 で実験自動化と粉体プロセスのインフォマティクスに関する招待講演を実施。',
+	},
+	{
+		date: 'Aug. 2025',
+		dateJa: '2025年8月',
+		html: 'Presented a poster at Accelerate Conference 2025 on imitation learning for robotic powder grinding.',
+		htmlJa: 'Accelerate Conference 2025 で粉体粉砕ロボットの模倣学習に関するポスター発表を実施。',
+	},
+	{
+		date: 'May 2024',
+		dateJa: '2024年5月',
+		html: 'Received the Best Demo Award at the ICRA 2024 workshop on AI and robotics for accelerated discovery.',
+		htmlJa: 'ICRA 2024 の AI and Robotics for Accelerated Discovery ワークショップで Best Demo Award を受賞。',
+	},
+	{
+		date: '2024',
+		dateJa: '2024年',
+		html: 'Published journal articles in <em>Digital Discovery</em> on robotic mechanochemical synthesis and autonomous powder X-ray diffraction.',
+		htmlJa: '<em>Digital Discovery</em> にロボットメカノケミカル合成と自律粉末X線回折に関する論文を掲載。',
+	},
+];
